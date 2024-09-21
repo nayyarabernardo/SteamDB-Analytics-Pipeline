bb6eb1f8b92a017cd7f9acc65ed7d24b39889090
Nayara <nayyarabernardo@gmail.com>
2024-09-21 16:45:28 -0300

    Atualização processingd

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,105 +1,128 @@
-import requests
+import os
+import glob
 import pandas as pd
-from bs4 import BeautifulSoup
 import datetime
-from google.oauth2 import service_account
+import logging
+
+# Configuração de logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def get_latest_file(directory: str, pattern: str) -> str:
+    """
+    Encontra o arquivo mais recente em um diretório que corresponde a um padrão específico.
+    
+    Args:
+        directory (str): O diretório onde procurar os arquivos.
+        pattern (str): O padrão do nome do arquivo (ex: 'steamdb_sales_raw_*.csv').
+    
+    Returns:
+        str: O caminho completo do arquivo mais recente ou None se nenhum arquivo for encontrado.
+    """
+
+    list_of_files = glob.glob(os.path.join(directory, pattern))
+    if not list_of_files:
+        return None
+    return max(list_of_files, key=os.path.getctime)
+
+def generate_filename(dataset_name: str, stage: str, extension: str = 'csv') -> str:
+    """
+    Gera um nome de arquivo com base no nome do dataset, estágio e data atual.
+    
+    Args:
+        dataset_name (str): Nome do dataset.
+        stage (str): Estágio do processamento (ex: 'raw', 'processed').
+        extension (str, optional): Extensão do arquivo. Padrão é 'csv'.
+    
+    Returns:
+        str: Nome do arquivo gerado.
+    """
+
+    date = datetime.datetime.now().strftime('%Y%m%d')
+    return f"{dataset_name}_{stage}_{date}.{extension}"
+
+"""
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    
+    #Aplica transformações ao DataFrame.
+    
+    #Args:
+    #    df (pd.DataFrame): DataFrame a ser transformado.
+    
+    #Returns:
+    #    pd.DataFrame: DataFrame transformado.
+    
+
+    df['discount_in_percent'] = df['discount_in_percent'].astype(int)
+    df['price_in_brl'] = df['price_in_brl'] / 100
+    
+    for col in ['end_time_in_seconds', 'start_time_in_seconds', 'release_time_in_seconds']:
+        df[col.replace('_in_seconds', '')] = pd.to_datetime(df[col], unit='s')
+        df = df.drop(columns=[col])
+    
+    df['all_time_low'] = pd.to_numeric(df['all_time_low'], errors='coerce')
+    
+    # Adiciona a coluna 'safra'
+    data_extracao = datetime.datetime.now()
+    df['safra'] = data_extracao.strftime('%Y%m')
+
+    return df
+"""
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Realiza transformações no DataFrame, incluindo a adição da coluna 'safra'.
+    
+    Args:
+        df (pd.DataFrame): O DataFrame original a ser transformado.
+    
+    Returns:
+        pd.DataFrame: O DataFrame transformado.
+    """
+
+    # Adiciona a coluna 'safra'
+    data_extracao = datetime.datetime.now()
+    df['safra'] = data_extracao.strftime('%Y%m')
+
+    return df
 
 
-# Autenticação no BigQuery
-key_file = r"C:\Users\nayya\Downloads\Estudo\projetos\desafio-beAnalytic-engdadosjr\credentials\projeto-key.json"
-credentials = service_account.Credentials.from_service_account_file(
-    key_file, scopes=["https://www.googleapis.com/auth/cloud-platform"]
-)
+def run_etl_pipeline(raw_directory: str, processed_directory: str):
+    """
+    Executa o pipeline ETL completo.
 
-# Scraping dos Dados
-headers = {
-    "authority": "steamdb.info",
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-    "cache-control": "max-age=0",
-    "content-type": "application/x-www-form-urlencoded",
-    "cookie": "cf_chl_2=6aea11f94091fb2; cf_clearance=JBW_rM4Qq2MaBk6xnMJG37RP.Va_DHBPfNysw6vM704-1688421692-0-160; __cf_bm=E0YlDKRYDk1pik0CjW6uyZc9IKW28NlMBDBuYc1Wirw-1688422242-0-ASuD4L7JwQy7SdOlANZHd2tZATnOii1GLnLXjy3UDC6N4jl73z8aAuGdkCW7Jb01hHGwwhKRu+nqyd21+RhEjfo=",
-    "dnt": "1",
-    "origin": "https://steamdb.info",
-    "referer": "https://steamdb.info/sales/?__cf_chl_tk=iOssqADThWuoQZf.pPHZ1ZMefyeuvSqIeK2Mi3i8ChA-1688343216-0-gaNycGzNDRA",
-    "sec-ch-ua": '^\^"Not.A/Brand^\^";v=^\^"8^\^", ^\^"Chromium^\^";v=^\^"114^\^", ^\^"Google Chrome^\^";v=^\^"114^\^"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "Windows",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-}
+    Args:
+        raw_directory (str): Diretório onde estão os arquivos raw.
+        processed_directory (str): Diretório onde serão salvos os arquivos processados.
+    """
+    
+    try:
+        latest_raw_file = get_latest_file(raw_directory, "steamdb_sales_raw_*.csv")
+        
+        if latest_raw_file is None:
+            logger.warning("Nenhum arquivo raw encontrado para processar.")
+            return
 
-data_request = requests.get("https://steamdb.info/sales/", headers=headers)
+        logger.info(f"Processando o arquivo: {latest_raw_file}")
+        
+        df = pd.read_csv(latest_raw_file)
+        df_transformed = transform_data(df)
 
+        
+        processed_filename = generate_filename('steamdb_sales', 'processed')
+        processed_filepath = os.path.join(processed_directory, processed_filename)
+        
+        df_transformed.to_csv(processed_filepath, index=False)
+        # Obtém o caminho relativo em relação ao diretório raiz
+        relative_path = os.path.relpath(processed_filepath, start='/home/nay/Documentos/Projetos')
 
-def extract_table_rows(html_data):
-    soup = BeautifulSoup(html_data, "html.parser")
-    items = soup.find_all("tr", class_="app")
-    return items
+        logger.info(f"Transformação concluída. Dados salvos em '{relative_path}'")
 
 
-def extract_data_from_rows(items):
-    items_data = []
+    except Exception as e:
+        logger.error(f"Erro durante o processamento: {str(e)}")
 
-    for item in items:
-        tds = item.find_all("td")
+if __name__ == "__main__":
+    RAW_DIRECTORY = "/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/data/raw/"
+    PROCESSED_DIRECTORY = "/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/data/processed/"
 
-        name = tds[2].text.strip("\n").split("\n\n")[0]
-        all_time_low = None
-        try:
-            all_time_low = float(
-                tds[2]
-                .text.strip("\n")
-                .split("\n\n")[1]
-                .split("$ ")[1]
-                .replace(",", ".")
-            )
-        except IndexError:
-            pass
-
-        other_fields = tds[3:]
-        other_fields_values = [field["data-sort"] for field in other_fields]
-        (
-            discount_in_percent,
-            price_in_brl,
-            rating_in_percent,
-            end_time_in_seconds,
-            start_time_in_seconds,
-            release_time_in_seconds,
-        ) = map(float, other_fields_values)
-
-        items_data.append(
-            {
-                "name": name,
-                "discount_in_percent": int(discount_in_percent),
-                "price_in_brl": price_in_brl / 100,
-                "all_time_low": all_time_low,
-                "rating_in_percent": rating_in_percent,
-                "end_time": datetime.datetime.fromtimestamp(end_time_in_seconds),
-                "start_time": datetime.datetime.fromtimestamp(start_time_in_seconds),
-                "release_time": datetime.datetime.fromtimestamp(
-                    release_time_in_seconds
-                ),
-            }
-        )
-
-    return items_data
-
-
-items = extract_table_rows(data_request.text)
-items_data = extract_data_from_rows(items)
-df = pd.DataFrame(items_data)
-
-# Enviar para o BigQuery
-destination_table = "projeto-beanalytic.steamdb.sales"
-df.to_gbq(
-    credentials=credentials, destination_table=destination_table, if_exists="replace"
-)
-
-# Salvar como CSV
-csv_file_path = r"C:\Users\nayya\Downloads\Estudo\projetos\desafio-beAnalytic-engdadosjr\data\sales.csv"
-df.to_csv(csv_file_path, index=False)
+    run_etl_pipeline(RAW_DIRECTORY, PROCESSED_DIRECTORY)
