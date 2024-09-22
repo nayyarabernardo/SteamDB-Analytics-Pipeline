717ebf431125d09ed16d463e7383d9d8ade6f435
Nayara <nayyarabernardo@gmail.com>
2024-09-21 22:38:51 -0300

    Scripts de Processamento e Carregamento

---
+++ automation/setup_cron.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define os caminhos para os scripts
+#EXTRACTION_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/extraction.py"
+PROCESSING_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/processing.py"
+MODELING_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/modeling/create_dim_games.py"
+#LOAD_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/load_to_bigquery.py"
+
+# Define a tarefa cron (aqui, execução diária às 19:00)
+CRON_JOB="50 19 * * * python3 $PROCESSING_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/processing_log.txt 2>&1 && \
+python3 $MODELING_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/modeling_log.txt 2>&1 &&"
+
+# Adiciona a tarefa cron
+( crontab -l; echo "$CRON_JOB" ) | crontab -
+
+# Mensagem de confirmação
+echo "Tarefa cron agendada para executar os scripts todos os dias às 19:00."
+
+
+#python3 $EXTRACTION_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/extraction_log.txt 2>&1 && \
+#python3 $PROCESSING_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/processing_log.txt 2>&1 && \
+#python3 $MODELING_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/modeling_log.txt 2>&1 && \
+#python3 $LOAD_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/load_log.txt 2>&1"
+
