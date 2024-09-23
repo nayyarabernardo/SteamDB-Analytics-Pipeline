5c60968b36c395ccc6c4648960b233ac0980bccf
Nayara <nayyarabernardo@gmail.com>
2024-09-23 19:29:24 -0300

    Modificações no codigo de agendamento

--- automation/setup_cron.sh
+++ automation/setup_cron.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+# Define o diretório base do projeto
+PROJECT_DIR="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline"
+
 # Define os caminhos para os scripts
-#EXTRACTION_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/extraction.py"
-PROCESSING_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/processing.py"
-MODELING_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/trusted.py"
-LOAD_SCRIPT="/home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/scripts/load_to_bigquery.py"
+PROCESSING_SCRIPT="$PROJECT_DIR/scripts/processing.py"
+MODELING_SCRIPT="$PROJECT_DIR/scripts/trusted.py"
+LOAD_SCRIPT="$PROJECT_DIR/scripts/load.py"
 
-# Define a tarefa cron (aqui, execução diária às 19:00)
-CRON_JOB="00 08 * * * python3 $PROCESSING_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/processing_log.txt 2>&1 && \
-python3 $MODELING_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/truste.txt 2>&1 \
-python3 $LOAD_SCRIPT >> /home/nay/Documentos/Projetos/SteamDB-Analytics-Pipeline/logs/load_log.txt 2>&1"
+# Caminho para o ambiente virtual existente
+VENV_DIR="$PROJECT_DIR/venv"  # Ajuste este caminho se necessário
 
+# Verifica se o ambiente virtual existe
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Erro: O ambiente virtual não foi encontrado em $VENV_DIR"
+    exit 1
+fi
+
+# Caminho para o Python do ambiente virtual
+PYTHON_PATH="$VENV_DIR/bin/python3"
+
+# Define a tarefa cron (execução diária às 17:00)
+CRON_JOB="50 12 * * * $PYTHON_PATH $PROCESSING_SCRIPT >> $PROJECT_DIR/logs/processing_log.txt 2>&1 && $PYTHON_PATH $MODELING_SCRIPT >> $PROJECT_DIR/logs/trusted_log.txt 2>&1 && $PYTHON_PATH $LOAD_SCRIPT >> $PROJECT_DIR/logs/load_log.txt 2>&1"
 
 # Adiciona a tarefa cron
-( crontab -l; echo "$CRON_JOB" ) | crontab -
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
 
 # Mensagem de confirmação
-echo "Tarefa cron agendada para executar os scripts todos os dias às 19:00."
-
-
-
+echo "Tarefa cron agendada para executar os scripts todos os dias às 17:00 usando o ambiente virtual em $VENV_DIR."
